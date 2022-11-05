01cc3318b5241cab329c34082bad689a4a3ede12
Lore <49278471+LoreWasTaken@users.noreply.github.com>
2022-11-05 19:51:15 +0000

    Fixed so now prints properly in the terminal

--- C++ Projects/practice/PracticeTest1F.cpp
+++ C++ Projects/practice/PracticeTest1F.cpp
@@ -4,21 +4,25 @@
 using namespace std;
 
 // b) Request number input
-int RequestNumber(int N){
+int RequestNumber()
+{
+    int N;
     do
     {
         cin >> N;
     } while (N < 3 || N > 15);
     return N;
 }
+
 // c) Sum of integers between 3 and number
-int sumIntegers(int N){
-    int sum = 0;
-    return sum = (3 + N) * (N - 3 + 1) / 2;
+int sumIntegers(int N)
+{
+    return (3 + N) * (N - 3 + 1) / 2;
 }
 
 // d) Print divisors of number
-int Divisors(int N){
+unsigned Divisors(int N)
+{
     unsigned divisors = 0;
     for (int i = 2; i <= N / 2; i++)
         if (N % i == 0)
@@ -30,41 +34,39 @@ int Divisors(int N){
 }
 
 // e) Times the number appears in the array numbers[]
-int Array(int numbers[10], int N)
+unsigned Array(int numbers[10], int N)
 {
     unsigned count = 0;
-    for (int i = 0; i < 10; i++)
+    for (size_t i = 0; i < 10; i++)
         if (numbers[i] == N)
             count++;
     return count;
 }
 
 // f) Comparison of the elements of the two arrays
-
-void moreNumbers(int moreNumbers[10], int N[15]){
+void moreNumbers(int numbers1[], int numbers2[])
+{
     for (size_t i = 0; i < 10; i++)
-        cout << setw(2) << N[i] << ' '
-             << (N[i] > moreNumbers[i] ? ">" : (N[i] < moreNumbers[i]) ? "<"
-                                                                       : "=")
-             << ' ' << setw(2) << moreNumbers[i];
+        cout << setw(2) << numbers2[i] << ' '
+             << ((numbers2[i] > numbers1[i]) ? '>' : (numbers2[i] < numbers1[i]) ? '<'
+                                                                                 : '=')
+             << ' ' << setw(2) << numbers1[i] << "\n";
 }
 
 int main()
 {
-    int N = RequestNumber(N);
-    int divisors = Divisors(N);
+    // Would print divisors too early
+    // unsigned divisors = Divisors(N);
     int numbers[]{2, 11, 5, 7, 14, 11, 7, 3, 11, 14};
-    unsigned count = Array(numbers, N);
-    int more_Numbers[]{1, 12, 4, 5, 15, 5, 13, 7, 12, 4};
-    int compare[10];
+    int more_numbers[]{1, 12, 4, 5, 15, 5, 13, 7, 12, 4};
 
     // a) Identification
     cout << "a) Denis Sukhachev, 114862\n";
 
     // b) Request number input
-    int number;
     cout << "b) Please input an integer in the interval [3, 15]: ";
-    cout << "N=" << RequestNumber(N);
+    int N = RequestNumber();
+    cout << "N=" << N << endl;
 
     // c) Sum of integers between 3 and number
     cout << "c) Sum of all integers between 3 and " << N
@@ -72,18 +74,21 @@ int main()
 
     // d) Print divisors of number
     cout << "d) Divisors of " << N << ": 1";
-    divisors;
-    (divisors == 0) ? cout << "The number is prime.\n" : cout << "The number is not prime.\n";
-    cout << endl;
+    unsigned divisors = Divisors(N);
+    cout << "\nThe number is ";
+    if (divisors > 0)
+        cout << "not ";
+    cout << "prime.\n";
 
     // e) Times the number appears in the array numbers[]
+    unsigned count = Array(numbers, N);
     cout << "e) The number " << N
          << " appears in the array {2, 11, 5, 7, 14, 11, 7, 3} "
-         << count << " times.\n";
+         << count << " time(s).\n";
 
     // f) Comparison of the elements of the two arrays
     cout << "f) Comparison of same index elements of two arrays.\n";
-    moreNumbers(more_Numbers, compare);
+    moreNumbers(numbers, more_numbers);
 
     return 0;
 }
